ca739e972406b678c5e4e57ec12ab309df62018f
SerMighty <luke.day19@hotmail.co.uk>
2019-12-16 13:38:39 +0000

    Added MPVinewood Dependancy, Various Fixes
    Fixes credited to Jammmo2k5

---
+++ mpheist3/__resource.lua
@@ -0,0 +1,80 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+dependencies {
+	'mpvinewood'
+}
+
+ files {
+  'data/carcols.meta',
+  'data/carvariations.meta',
+  'data/clip_sets.xml',
+  'data/handling.meta',
+  'data/vehiclelayouts.meta',
+  'data/vehiclelayouts_formula.meta',
+  'data/vehiclelayouts_zhaba.meta',
+  'data/vehicles.meta',
+  'audio/dlcheist3_amp.dat10',
+  'audio/dlcheist3_amp.dat10.nametable',
+  'audio/dlcheist3_amp.dat10.rel',
+  'audio/dlcheist3_game.dat151',
+  'audio/dlcheist3_game.dat151.nametable',
+  'audio/dlcheist3_game.dat151.rel',
+  'audio/dlcheist3_sounds.dat54',
+  'audio/dlcheist3_sounds.dat54.nametable',
+  'audio/dlcheist3_sounds.dat54.rel',
+  'audio/sfx/dlc_heist3/asbo.awc',
+  'audio/sfx/dlc_heist3/asbo_npc.awc',
+  'audio/sfx/dlc_heist3/everon.awc',
+  'audio/sfx/dlc_heist3/everon_npc.awc',
+  'audio/sfx/dlc_heist3/formula.awc',
+  'audio/sfx/dlc_heist3/formula2.awc',
+  'audio/sfx/dlc_heist3/formula2_npc.awc',
+  'audio/sfx/dlc_heist3/formula_npc.awc',
+  'audio/sfx/dlc_heist3/furia.awc',
+  'audio/sfx/dlc_heist3/furia_npc.awc',
+  'audio/sfx/dlc_heist3/imorgon.awc',
+  'audio/sfx/dlc_heist3/imorgon_npc.awc',
+  'audio/sfx/dlc_heist3/jb7002.awc',
+  'audio/sfx/dlc_heist3/jb7002_npc.awc',
+  'audio/sfx/dlc_heist3/kanjo.awc',
+  'audio/sfx/dlc_heist3/kanjo_npc.awc',
+  'audio/sfx/dlc_heist3/komoda.awc',
+  'audio/sfx/dlc_heist3/komoda_npc.awc',
+  'audio/sfx/dlc_heist3/minitank.awc',
+  'audio/sfx/dlc_heist3/outlaw.awc',
+  'audio/sfx/dlc_heist3/outlaw_npc.awc',
+  'audio/sfx/dlc_heist3/rebla.awc',
+  'audio/sfx/dlc_heist3/rebla_npc.awc',
+  'audio/sfx/dlc_heist3/retinue2.awc',
+  'audio/sfx/dlc_heist3/retinue2_npc.awc',
+  'audio/sfx/dlc_heist3/stryder.awc',
+  'audio/sfx/dlc_heist3/stryder_npc.awc',
+  'audio/sfx/dlc_heist3/sugoi.awc',
+  'audio/sfx/dlc_heist3/sugoi_npc.awc',
+  'audio/sfx/dlc_heist3/sultan2.awc',
+  'audio/sfx/dlc_heist3/sultan2_npc.awc',
+  'audio/sfx/dlc_heist3/vagrant.awc',
+  'audio/sfx/dlc_heist3/vagrant_npc.awc',
+  'audio/sfx/dlc_heist3/vstr.awc',
+  'audio/sfx/dlc_heist3/vstr_npc.awc',
+  'audio/sfx/dlc_heist3/yosemite2.awc',
+  'audio/sfx/dlc_heist3/yosemite2_npc.awc',
+  'audio/sfx/dlc_heist3/zhaba.awc',
+  'audio/sfx/dlc_heist3/zhaba_npc.awc',
+}
+
+data_file 'AUDIO_GAMEDATA' 'audio/dlcheist3_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/dlcheist3_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'audio/dlcheist3_amp.dat'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'CLIP_SETS_FILE' 'data/clip_sets.xml'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_formula.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_zhaba.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_heist3'
+
+client_script 'names.lua'
+
---
+++ mpvinewood/__resource.lua
@@ -0,0 +1,91 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+ dependencies {
+} 
+
+ files {
+  'data/carcols.meta',
+  'data/carvariations.meta',
+  'data/clip_sets.xml',
+  'data/names.lua',
+  'data/handling.meta',
+  'data/vehiclelayouts.meta',
+  'data/vehiclelayouts_rrocket.meta',
+  'data/vehiclelayouts_s80.meta',
+  'data/vehicles.meta',
+  'data/vehicleweapons_paragon2.meta',
+  'audio/dlcvinewood_amp.dat10',
+  'audio/dlcvinewood_amp.dat10.nametable',
+  'audio/dlcvinewood_amp.dat10.rel',
+  'audio/dlcvinewood_game.dat151',
+  'audio/dlcvinewood_game.dat151.nametable',
+  'audio/dlcvinewood_game.dat151.rel',
+  'audio/dlcvinewood_mix.dat15',
+  'audio/dlcvinewood_mix.dat15.nametable',
+  'audio/dlcvinewood_mix.dat15.rel',
+  'audio/dlcvinewood_sounds.dat54',
+  'audio/dlcvinewood_sounds.dat54.nametable',
+  'audio/dlcvinewood_sounds.dat54.rel',
+  'audio/dlcvinewood_speech.dat4',
+  'audio/dlcvinewood_speech.dat4.nametable',
+  'audio/dlcvinewood_speech.dat4.rel',
+  'audio/sfx/dlc_vinewood/caracara2.awc',
+  'audio/sfx/dlc_vinewood/caracara2_npc.awc',
+  'audio/sfx/dlc_vinewood/drafter.awc',
+  'audio/sfx/dlc_vinewood/drafter_npc.awc',
+  'audio/sfx/dlc_vinewood/dynasty.awc',
+  'audio/sfx/dlc_vinewood/dynasty_npc.awc',
+  'audio/sfx/dlc_vinewood/emerus.awc',
+  'audio/sfx/dlc_vinewood/emerus_npc.awc',
+  'audio/sfx/dlc_vinewood/gauntlet3.awc',
+  'audio/sfx/dlc_vinewood/gauntlet3_npc.awc',
+  'audio/sfx/dlc_vinewood/gauntlet4.awc',
+  'audio/sfx/dlc_vinewood/gauntlet4_npc.awc',
+  'audio/sfx/dlc_vinewood/hellion.awc',
+  'audio/sfx/dlc_vinewood/hellion_npc.awc',
+  'audio/sfx/dlc_vinewood/issi7.awc',
+  'audio/sfx/dlc_vinewood/issi7_npc.awc',
+  'audio/sfx/dlc_vinewood/jugular.awc',
+  'audio/sfx/dlc_vinewood/jugular_npc.awc',
+  'audio/sfx/dlc_vinewood/krieger.awc',
+  'audio/sfx/dlc_vinewood/krieger_npc.awc',
+  'audio/sfx/dlc_vinewood/locust.awc',
+  'audio/sfx/dlc_vinewood/locust_npc.awc',
+  'audio/sfx/dlc_vinewood/nebula.awc',
+  'audio/sfx/dlc_vinewood/nebula_npc.awc',
+  'audio/sfx/dlc_vinewood/neo.awc',
+  'audio/sfx/dlc_vinewood/neo_npc.awc',
+  'audio/sfx/dlc_vinewood/novak.awc',
+  'audio/sfx/dlc_vinewood/novak_npc.awc',
+  'audio/sfx/dlc_vinewood/paragon.awc',
+  'audio/sfx/dlc_vinewood/paragon_npc.awc',
+  'audio/sfx/dlc_vinewood/peyote2.awc',
+  'audio/sfx/dlc_vinewood/peyote2_npc.awc',
+  'audio/sfx/dlc_vinewood/rrocket.awc',
+  'audio/sfx/dlc_vinewood/rrocket_npc.awc',
+  'audio/sfx/dlc_vinewood/s80.awc',
+  'audio/sfx/dlc_vinewood/s80_npc.awc',
+  'audio/sfx/dlc_vinewood/thrax.awc',
+  'audio/sfx/dlc_vinewood/thrax_npc.awc',
+  'audio/sfx/dlc_vinewood/zion3.awc',
+  'audio/sfx/dlc_vinewood/zion3_npc.awc',
+  'audio/sfx/dlc_vinewood/zorrusso.awc',
+  'audio/sfx/dlc_vinewood/zorrusso_npc.awc',
+}
+
+data_file 'AUDIO_GAMEDATA' 'audio/dlcvinewood_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audio/dlcvinewood_sounds.dat'
+data_file 'AUDIO_SYNTHDATA' 'audio/dlcvinewood_amp.dat'
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'CLIP_SETS_FILE' 'data/clip_sets.xml'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_rrocket.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/vehiclelayouts_s80.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'WEAPONINFO_FILE' 'data/vehicleweapons_paragon2.meta'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/dlc_vinewood'
+
+client_script 'data/names.lua'
+
